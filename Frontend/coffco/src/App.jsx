37b11cd0d820faf47eba719768e1cffceee510e8
daniel22alejandro <danniel10yara@gmail.com>
2024-05-23 09:40:27 -0500

    cambios en mi rama para comparar y corregir

--- Frontend/coffco/src/App.jsx
+++ Frontend/coffco/src/App.jsx
@@ -1,27 +1,26 @@
 import React from "react";
+import {Route, Routes } from "react-router-dom";
 
-//importar pages
+import LoginPage from './pages/loginPage';
+import RegistroPage from "./pages/RegistroPage";
+import Inicio from './pages/inicio';
+import AdministrarUsurio from './pages/administrarUsuarios';
+import TrillaPage from "./pages/TrillaPage";
 
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import RegistroPage from './pages/RegistroPage';
-import RegistroComponent from "./components/RegistroComponent";
 
-//importar components
+
+
 
 const App = () => {
 return (
 <>
-<Router>
-      <Switch>
-        <Route exact path="/">
-          {/* Página de inicio */}
-        </Route>
-        <Route exact path="/registro">
-          <RegistroPage />
-        </Route>
-        {/* Otras rutas */}
-      </Switch>
-    </Router>
+<Routes>
+        <Route path="/" element={<LoginPage />} />  
+        <Route path="/registro" element={<RegistroPage />} />  
+        <Route exact path="/inicio" element={<Inicio/>}></Route>
+        <Route exact path="/administarusuario" element={<AdministrarUsurio/>}></Route>
+        <Route path="/trilla" element={<TrillaPage/>}></Route>
+</Routes>
 </>
 );
 };
